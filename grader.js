7acec4853d5dbc1034ddca4ddb23ec50b4b6e7c9
Ubuntu <ubuntu@ip-172-31-1-188.us-west-2.compute.internal>
2013-07-11 01:11:30 +0000

    Modified grader.js

--- grader.js
+++ grader.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env/node
+#!/usr/bin/node
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
@@ -9,10 +9,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "https://floating-harbor-9271.herokuapp.com/"
 
-var assertFileExists = function(inFile) {
+var assertFileExists = function(infile) {
     var instr = infile.toString();
     if (!fs.existsSync(instr)){
 	console.log("%s does not exist. Exiting.", instr);
@@ -21,7 +24,7 @@ var assertFileExists = function(inFile) {
     return instr;
 };
 
-var cherrioHtmlFile = function(htmlfile) {
+var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
@@ -48,6 +51,7 @@ if(require.main == module){
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL of HTML file', clone(assertFileExists), URL_DEFAULT)  
     .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
